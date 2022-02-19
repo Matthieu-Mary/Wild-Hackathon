514cbfd7f843df18fbb74d89c225db2af22951a5
Matthieu Mary <matthieu.mary2@gmail.com>
2022-02-19 11:30:31 +0100

    modify NavBar structure

---
+++ wild-hackathon/src/Components/QrGenerator/QrGenetator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function QrGenetator() {
+  return (
+    <div>
+        <NavBar />
+        <QrForm />
+        <QrButton />
+        <QrList />
+    </div>
+  )
+}
