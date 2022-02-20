59531978dc6897377b33332c588a8798045c8fcc
Matthieu Mary <matthieu.mary2@gmail.com>
2022-02-20 09:12:33 +0100

    add NavBar JSX

--- wild-hackathon/src/App.js
+++ wild-hackathon/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './Components/NavBar/NavBar'
 import QrGenerator from "./Components/QrGenerator/QrGenetator"
 import ContainerCarte from './Components/Menus/ContainerCarte';
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
